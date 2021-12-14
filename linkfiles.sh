779980d19d371022c68917dc6b4296f1890be770
Hou Yong Chan <chanhouyong@sea.com>
2021-12-15 02:06:25 +0800

    added vimrc

--- linkfiles.sh
+++ linkfiles.sh
@@ -5,3 +5,4 @@ cur_dir=$(pwd)
 ln -s -f $cur_dir/dotfiles/p10k.zsh ~/.p10k.zsh
 ln -s -f $cur_dir/dotfiles/zshrc ~/.zshrc
 ln -s -f $cur_dir/dotfiles/tmux.conf ~/.tmux.conf
+ln -s -f $cur_dir/dotfiles/vimrc ~/.vimrc
