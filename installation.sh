84d9a0cf629b485d6b4e5da8ab810ac4ce93268c
chyy <19471963+chy1011@users.noreply.github.com>
2022-01-10 20:58:33 +0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -27,7 +27,7 @@ echo "============================"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
 ln -s -f $cur_dir/dotfiles/zshrc ~/.zshrc
 
